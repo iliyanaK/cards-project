e84339d4944762ba56b4456c2dfbafc0b32afc83
IliyanaK <iliyana@izpitai.me>
2020-06-21 18:23:44 +0300

    Added sum and sort actions and the relevant reducer's resolver +
    handle error state

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,32 +1,98 @@
+import { chain } from "lodash";
+
 // types of action
 export const ACTIONS = {
   SET_DECK: "SET_DECK",
   SET_CARDS: "SET_CARDS",
-  SORT_DECK: "SORT_DECK"
+  SORT_DECK: "SORT_DECK",
+  SUM_DECK: "SUM_DECK",
+  SET_LOADING: "SET_LOADING",
 };
 
 // actions
-export function getDeck() {
-  return async dispatch => {
-    const deck = await fetch("/api/deck/new/shuffle/?deck_count=1").then(
-      response => response.json()
-    );
-    const cards = await fetch(
-      `/api/deck/${deck.deck_id}/draw/?count=${deck.remaining}`
-    ).then(response => response.json());
+export const setLoading = (loading) => ({
+  type: ACTIONS.SET_LOADING,
+  payload: {
+    loading,
+  },
+});
 
-    dispatch({
-      type: ACTIONS.SET_CARDS,
-      payload: {
-        deck: cards
+export const getDeck = () => async (dispatch) => {
+  dispatch(setLoading(true));
+
+  const deck = await fetch(
+    "/api/deck/new/shuffle/?deck_count=1"
+  ).then((response) => response.json());
+
+  dispatch({
+    type: ACTIONS.SET_DECK,
+    payload: {
+      deck,
+    },
+  });
+};
+
+export const getCards = (deckId, remaining) => async (dispatch) => {
+  dispatch(setLoading(true));
+
+  const cardsData = await fetch(
+    `/api/deck/${deckId}/draw/?count=${remaining}`
+  ).then((response) => response.json());
+
+  dispatch({
+    type: ACTIONS.SET_CARDS,
+    payload: {
+      cardsData,
+    },
+  });
+};
+
+export const sumDeck = () => (dispatch, getState) => {
+  const sum = chain(getState().cards)
+    .filter(({ suit }) => ["CLUBS", "SPADES"].includes(suit))
+    .reduce((acc, { value }) => {
+      let weight = parseInt(value);
+      if (["JACK", "KING", "QUEEN"].includes(value)) {
+        weight = 10;
+      } else if (value === "ACE") {
+        weight = 1;
       }
-    });
-  };
-}
+      return acc + weight;
+    }, 0)
+    .value();
 
-export function sumDeck() {
-  return dispatch => {};
-}
-export function sortDeck() {
-  return dispatch => {};
-}
+  dispatch({
+    type: ACTIONS.SUM_DECK,
+    payload: {
+      sum,
+    },
+  });
+};
+
+export const sortDeck = () => (dispatch, getState) => {
+  const sortedDeck = chain(getState().cards)
+    .map((card) => {
+      let weight = parseInt(card.value);
+      if (card.value === "JACK") {
+        weight = 11;
+      } else if (card.value === "QUEEN") {
+        weight = 12;
+      } else if (card.value === "KING") {
+        weight = 13;
+      } else if (card.value === "ACE") {
+        weight = 14;
+      }
+      return { ...card, weight };
+    })
+    .sortBy(["suit", "weight"])
+    .value();
+
+  dispatch({
+    type: ACTIONS.SET_CARDS,
+    payload: {
+      cardsData: {
+        cards: sortedDeck,
+      },
+    },
+  });
+};
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,21 +2,50 @@ import { ACTIONS } from "./action";
 
 const defaultState = {
   deck: {
-    has_cards: false
-  }
+    has_cards: false,
+  },
+  cards: [],
+  loading: true,
+  errorMessage: '',
+  sum: 0,
 };
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case ACTIONS.SET_CARDS: {
-      const { deck, cards } = action.payload;
+    case ACTIONS.SET_LOADING: {
+      const { loading } = action.payload;
+      return {
+        ...state,
+        loading,
+      };
+    }
+    case ACTIONS.SET_DECK: {
+      const { deck } = action.payload;
       return {
         ...state,
         deck,
-        cards
+        loading: false,
+        errorMessage: deck.error,
       };
     }
-
+    case ACTIONS.SET_CARDS: {
+      const { cardsData } = action.payload;
+      return {
+        ...state,
+        cards: cardsData.cards,
+        loading: false,
+        errorMessage: cardsData.error,
+      };
+    }
+    case ACTIONS.SUM_DECK: {
+      const { sum } = action.payload;
+      return {
+        ...state,
+        sum,
+        loading: false,
+        errorMessage: '',
+      };
+    }
     default:
       return state;
   }
