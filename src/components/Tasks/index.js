d06069fc1e86be0030f3370a248a2e46f8739925
IliyanaK <iliyana@izpitai.me>
2020-06-21 15:28:50 +0300

    Update Tasks styling and call sum and sort actions

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -2,53 +2,57 @@ import React from "react";
 import { connect } from "react-redux";
 import { sortDeck, sumDeck } from "../../redux/action";
 
+import "./Tasks.scss";
+
 class Tasks extends React.Component {
   sortDeck() {
     this.props.sortDeck();
   }
 
   async sumDeck() {
-    const sum = await this.props.sumDeck();
-    alert(sum);
+    await this.props.sumDeck();
+    alert(this.props.sum);
   }
 
   render() {
     return (
-      <div className="flex justify-center items-start mw8">
-        <div className="fl w-50 tl pa1">
+      <div className="flex justify-center">
+        <div className="fl task-box tl pa1">
           <div className="ba b--near-white pv1 ph2">
             <h1>Task 1 - Add up black cards</h1>
             <p>
               Clicking the "Sum Black Cards" button should trigger an alert,
               which shows the sum of the value of all of the cards:
-              <ol>
-                <li>A = 1, J,Q,K = 10</li>
-              </ol>
             </p>
+            <ol>
+              <li>A = 1, J,Q,K = 10</li>
+            </ol>
             <button
               onClick={() => this.sumDeck()}
               className="bg-animate bg-dark-blue inline-flex items-center ma2 tc bw0 white b pa2 dim"
+              disabled={this.props.loading || this.props.error}
             >
               Sum Black Cards
             </button>
           </div>
         </div>
-        <div className="fl w-50 tl pa1">
+        <div className="fl task-box tl pa1">
           <div className="ba b--near-white pv1 ph2">
             <h1>Task 2 - Sort Cards</h1>
             <p>
               Clicking the "Sort Cards" button should sort the deck into order,
               so they appear:
-              <ol>
-                <li>Grouped into Suite (Clubs, Diamonds, Hearts, Spades)</li>
-                <li>
-                  In ascending order (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A).
-                </li>
-              </ol>
             </p>
+            <ol>
+              <li>Grouped into Suite (Clubs, Diamonds, Hearts, Spades)</li>
+              <li>
+                In ascending order (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A).
+              </li>
+            </ol>
             <button
               onClick={() => this.sortDeck()}
               className="bg-animate bg-dark-blue inline-flex items-center ma2 tc bw0 white b pa2 dim"
+              disabled={this.props.loading || this.props.error}
             >
               Sort Cards
             </button>
@@ -59,7 +63,10 @@ class Tasks extends React.Component {
   }
 }
 
-export default connect(
-  null,
-  { sortDeck, sumDeck }
-)(Tasks);
+const mapPropsToState = ({ sum, loading, error }) => ({
+  sum,
+  loading,
+  error,
+});
+
+export default connect(mapPropsToState, { sortDeck, sumDeck })(Tasks);
