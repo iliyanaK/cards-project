1cc3c4578c7293ef37e6866e58982df3c21a10e5
IliyanaK <iliyana@izpitai.me>
2020-06-21 15:29:42 +0300

    Get deck and redirect to cards page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,49 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { getDeck } from "../../redux/action";
-import Tasks from "../Tasks";
-import Cards from "../Cards";
 
-const App = ({ deck, getDeck }) => (
-  <div className="tc flex flex-column items-center justify-center">
-    {deck ? (
-      <>
-        <Tasks />
-        <Cards cards={deck.cards} />
-      </>
-    ) : (
-      <>
-        <div className="lightest-blue tl">
-          <p>
-            Currently, clicking "Fetch Cards" multiple times sends multiple
-            requests to API.
-          </p>
-          <p className="b">
-            Task: Add loading component, so we can only make one request at a
-            time.
-          </p>
-        </div>
-        <button
-          onClick={() => getDeck()}
-          className="bg-animate bg-blue hover-bg-light-blue inline-flex items-center bw0 ma1 tc navy pv2 ph6 br4 f3"
-        >
-          Fetch Cards
-        </button>
-        <p className="i f6 light-blue">
-          Note: extra credit will be given for refactoring/suggestions done
-          throughout.
-        </p>
-      </>
-    )}
-  </div>
-);
+const App = ({ deck, getDeck, loading }) => {
+  /**
+   * Since the remaining is always 52, by replacing the count search param with the below random generated number
+   * we can check the behaviour on refresh with dynamic 'remaining' value
+   */
+  // const count = Math.floor(Math.random() * (52 - 3)) + 3;
 
-const mapStateToProps = ({ deck }) => ({
-  deck
+  useEffect(() => {
+    getDeck();
+  }, [getDeck]);
+
+  return (
+    <div className="tc flex flex-column items-center justify-center">
+      {deck && (
+        <>
+          <div className="lightest-blue tl">
+            <p>
+              Currently, clicking "Fetch Cards" multiple times sends multiple
+              requests to API.
+            </p>
+            <p className="b">
+              Task: Add loading component, so we can only make one request at a
+              time.
+            </p>
+          </div>
+          <Link
+            to={{
+              pathname: `/cards/${deck.deck_id}`,
+              search: `?count=${deck.remaining}`,
+            }}
+            className="bg-animate bg-blue hover-bg-light-blue inline-flex items-center bw0 ma1 tc navy pv2 ph6 br4 f3"
+            disabled={loading}
+          >
+            Fetch Cards
+          </Link>
+          <p className="i f6 light-blue">
+            Note: extra credit will be given for refactoring/suggestions done
+            throughout.
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = ({ deck, loading }) => ({
+  deck,
+  loading,
 });
 
-export default connect(
-  mapStateToProps,
-  { getDeck }
-)(App);
+export default connect(mapStateToProps, { getDeck })(App);
